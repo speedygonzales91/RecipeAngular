1641eb6f083b8ed077558a2edb6a34ad0c5d76ed
Molnár Zsolt <Molnar.Zsolt@ArgonSoft.hu>
2019-11-28 12:02:00 +0100

    06 Add property binding to recipe data

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -13,6 +13,8 @@ export class RecipeItemComponent implements OnInit {
     new Recipe('Test Recipe','This is simply a test','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg') 
   ];
   */
+
+  @Input() recipe : Recipe;
   constructor() { }
 
   ngOnInit() {
