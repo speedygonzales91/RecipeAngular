e0036f1c4a51d372af05dab4dce4a8619346a0c3
Molnár Zsolt <Molnar.Zsolt@ArgonSoft.hu>
2019-12-05 09:49:00 +0100

    10 Passing ingredients between services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,50 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
    public recipeWasSelected = new EventEmitter<Recipe>(); 
    private recipes : Recipe[] = [
-        new Recipe('Test Recipe1','This is simply a test1','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Test Recipe2','This is simply a test2','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Test Recipe3','This is simply a test3','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+        new Recipe(
+          'Test Recipe1',
+          'This is simply a test1',
+          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+          [
+            new Ingredient('Ingredient1',10),
+            new Ingredient('Ingredient2',20),
+          ]
+          ),
+        new Recipe(
+          'Test Recipe2',
+          'This is simply a test2',
+          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+          [
+            new Ingredient('Ingredient3',30),
+            new Ingredient('Ingredient4',40),
+          ]
+          ),
+        new Recipe(
+          'Test Recipe3',
+          'This is simply a test3',
+          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+          [
+            new Ingredient('Ingredient5',50),
+            new Ingredient('Ingredient6',60),
+          ]
+          )
       ];
+
+
+    constructor(private slService :ShoppingListService){}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients :Ingredient[]) {
+      this.slService.addIngredientsFromRecipe(ingredients);
     }
 }
